da73c7e2090f8d003650bf1d50922fee3211d853
jaam8 <jaam8@yandex.ru>
2025-05-24 00:33:37 +0700

    feat: implement storage adapter

--- internal/ports/adapters/storage/storage.go
+++ internal/ports/adapters/storage/storage.go
@@ -1,34 +1,100 @@
 package storage
 
 import (
+	"errors"
 	"quotes/internal/models"
+	errs "quotes/pkg/errors"
 	"sync"
 )
 
 type DBAdapter struct {
-	db map[uint64]*models.Quote
-	mu sync.Mutex
+	idCounter uint64
+	byId      map[uint64]*models.Quote
+	byAuthor  map[string][]*models.Quote
+	mu        sync.RWMutex
 }
 
 func NewDBAdapter() *DBAdapter {
 	return &DBAdapter{
-		db: make(map[uint64]*models.Quote),
-		mu: sync.Mutex{},
+		idCounter: 0,
+		byId:      make(map[uint64]*models.Quote),
+		byAuthor:  make(map[string][]*models.Quote),
+		mu:        sync.RWMutex{},
 	}
 }
 
-func (db *DBAdapter) CreateQuote(quote *models.Quote) (uint64, error) {
-	return 0, nil
+func (db *DBAdapter) SaveQuote(quote *models.Quote) (uint64, error) {
+	if quote == nil {
+		return 0, errors.New("quote is nil")
+	}
+
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	db.idCounter += 1
+	quote.Id = db.idCounter
+
+	db.byId[db.idCounter] = quote
+	db.byAuthor[quote.Author] = append(db.byAuthor[quote.Author], quote)
+
+	return quote.Id, nil
 }
 
 func (db *DBAdapter) GetQuote(id uint64) (*models.Quote, error) {
-	return nil, nil
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	quote, ok := db.byId[id]
+	if !ok {
+		return nil, errs.ErrQuoteNotFound
+	}
+
+	return quote, nil
 }
 
 func (db *DBAdapter) GetQuotes(author *string) ([]*models.Quote, error) {
-	return nil, nil
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	if author == nil {
+		var quotes []*models.Quote
+		for _, q := range db.byId {
+			quotes = append(quotes, q)
+		}
+		return quotes, nil
+	}
+
+	quotes, ok := db.byAuthor[*author]
+	if !ok {
+		return []*models.Quote{}, nil
+	}
+
+	return quotes, nil
 }
 
-func (db *DBAdapter) DeleteQuote(id uint64) (*models.Quote, error) {
-	return nil, nil
+func (db *DBAdapter) DeleteQuote(id uint64) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	quote, ok := db.byId[id]
+	if !ok {
+		return errs.ErrQuoteNotFound
+	}
+	delete(db.byId, id)
+
+	quotes := db.byAuthor[quote.Author]
+	for i, q := range db.byAuthor[quote.Author] {
+		if q.Id == quote.Id {
+			quotes = append(quotes[:i], quotes[i+1:]...)
+			break
+		}
+	}
+
+	if len(quotes) == 0 {
+		delete(db.byAuthor, quote.Author)
+	} else {
+		db.byAuthor[quote.Author] = quotes
+	}
+
+	return nil
 }
--- internal/ports/ports.go
+++ internal/ports/ports.go
@@ -3,8 +3,8 @@ package ports
 import "quotes/internal/models"
 
 type StorageAdapter interface {
-	CreateQuote(quote *models.Quote) (uint64, error)
+	SaveQuote(quote *models.Quote) (uint64, error)
 	GetQuote(id uint64) (*models.Quote, error)
 	GetQuotes(author *string) ([]*models.Quote, error)
-	DeleteQuote(id uint64) (*models.Quote, error)
+	DeleteQuote(id uint64) error
 }
