00fa85b135ed7ea721275649dd79843a9e392ae8
jaam8 <jaam8@yandex.ru>
2025-05-24 02:08:34 +0700

    feat: add logging middleware

---
+++ internal/http/logging_middleware.go
@@ -0,0 +1,27 @@
+package http
+
+import (
+	"log"
+	"net/http"
+)
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+
+		sw := &statusWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		next.ServeHTTP(w, r)
+
+		log.Printf("Response: %s %s %d", r.Method, r.URL.Path, sw.statusCode)
+	})
+}
+
+type statusWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *statusWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
