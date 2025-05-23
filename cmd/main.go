74d05977c10a85e28612c1fb58bbee337510cefc
jaam8 <jaam8@yandex.ru>
2025-05-24 02:35:16 +0700

    feat: add initalization for db_adapter and quotes_service, add logging middleware to setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"quotes/internal/config"
 	h "quotes/internal/http"
+	"quotes/internal/ports/adapters/storage"
+	"quotes/internal/service"
 )
 
 func main() {
+
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DBAdapter := storage.NewDBAdapter()
+	srv, err := service.NewQuotesService(DBAdapter)
+	handler, err := h.NewHandler(srv)
+
 	r := mux.NewRouter()
 
-	handler := h.NewHandler()
+	r.Use(h.LogMiddleware)
 
 	r.HandleFunc("/quotes", handler.CreateQuote).Methods("POST")
 	r.HandleFunc("/quotes", handler.GetQuotes).Methods("GET")
-	r.HandleFunc("/quotes/random", handler.GetRandomQuote).Methods("PUT")
-	r.HandleFunc("/quotes/:id", handler.DeleteQuote).Methods("DELETE")
+	r.HandleFunc("/quotes/random", handler.GetRandomQuote).Methods("GET")
+	r.HandleFunc("/quotes/{id}", handler.DeleteQuote).Methods("DELETE")
 
-	log.Println("Server running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Printf("Server running at http://localhost:%d", cfg.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r))
 }
